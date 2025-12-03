c2ab0f0ac0ff5a2e7fdba0c53cdf0c5fb9596def
Prayanshu Singh Gaur <Prayanshusingh36@gmail.com>
2025-12-04 00:13:08 +0530

    Time: 45 ms (34.21%) | Memory: 90.9 MB (24.22%) - LeetSync

---
+++ 164-maximum-gap/maximum-gap.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maximumGap(int[] nums) {
+        if (nums == null || nums.length < 2){
+            return 0;
+        }
+        
+        Arrays.sort(nums);
+        int maxDiff = 0;
+        for (int i = 1; i < nums.length; i++) {
+            int diff = nums[i] - nums[i - 1];
+            if (diff > maxDiff) {
+                maxDiff = diff;
+            }
+        }
+        return maxDiff;
+    }
+}
