a1f87f41762bd3e939f1ed1392ef067355c315a5
Prayanshu Singh Gaur <Prayanshusingh36@gmail.com>
2025-12-09 23:32:24 +0530

    Time: 53 ms (19.75%) | Memory: 53.2 MB (93.19%) - LeetSync

---
+++ 2796-allow-one-function-call/allow-one-function-call.js
@@ -0,0 +1,27 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function(fn) {
+    let cnt=0;
+    return function(...args){
+        if(cnt==0)
+        {
+            cnt++;
+            return fn(...args);
+        }
+        else
+        {
+            return undefined;
+        }
+    }
+//please upvote...
+};
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
