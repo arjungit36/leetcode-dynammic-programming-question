101e3bb316da664146e4cd1d358dda617a24447d
Prayanshu Singh Gaur <Prayanshusingh36@gmail.com>
2026-06-12 18:43:07 +0530

    Time: 1 ms (100.00%) | Memory: 42.7 MB (60.95%) - LeetSync

--- 441-arranging-coins/arranging-coins.java
+++ 441-arranging-coins/arranging-coins.java
@@ -1,16 +1,5 @@
 class Solution {
-    //  public int mySqrt(long n) {
-    //     if(n==0) return 0;
-    //      long low=1,high=n;
-    //     while(low<=high){
-    //         long mid = low+(high-low )/2;
-    //         if(mid == (n/mid)) return (int)mid;
-    //         else if(mid >n/mid)
-    //             high=mid-1;
-    //         else
-    //             low=mid+1;
-    //     }return (int)high;
-    //  }
+
     public int arrangeCoins(int n){
         long m=(long)n;
         return (int)(Math.sqrt(1+8*m)-1)/2;
